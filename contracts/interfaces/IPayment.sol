cbab2ea0b4ebc725e3fb5af6c44a5f4f22844bc8
0xcodercrane <codercrane@gmail.com>
2022-07-22 03:00:01 +0800

    payment contract

--- contracts/interfaces/IPayment.sol
+++ contracts/interfaces/IPayment.sol
@@ -7,19 +7,19 @@ pragma solidity 0.8.13;
  * @param token - The payment token address
  * @param to - The receiver address
  * @param amount - The token amount
- * @param validators - The validator nodes who validate the payment amount, token and the receiver.
- * @param signatures - The sign messages of validators to verify the transaction
+ * @param validators - The validator address who validates the payment amount, token and the receiver.
+ * @param signatures - The sign messages of the validator to verify the transaction
  */
 struct PayParams {
   address token;
   address to;
   uint256 amount;
-  address[] validators;
-  bytes[] signatures;
+  address validator;
+  bytes signature;
 }
 
 interface IPayment {
   function deposit(address token, uint256 amount) external;
 
-  function pay(PayParams params) external;
+  function pay(PayParams calldata params) external;
 }
